8465b7111d6c3e8b58ba17474a57cdd6e62e1ae4
qianchenY <yangkuanmeng@gmail.com>
2026-07-29 16:53:37 +0800

    重构滚动动画函数并完善开发规范

--- src/js/main.js
+++ src/js/main.js
@@ -11,9 +11,116 @@ var baguetteBox=function(){function t(t,n){L.transforms=f(),L.svg=p(),e(),D=docu
 /*! Lazy Load 1.9.3 - MIT license - Copyright 2010-2013 Mika Tuupola */
 !function(a,b,c,d){var e=a(b);a.fn.lazyload=function(f){function g(){var b=0;i.each(function(){var c=a(this);if(!j.skip_invisible||c.is(":visible"))if(a.abovethetop(this,j)||a.leftofbegin(this,j));else if(a.belowthefold(this,j)||a.rightoffold(this,j)){if(++b>j.failure_limit)return!1}else c.trigger("appear"),b=0})}var h,i=this,j={threshold:0,failure_limit:0,event:"scroll",effect:"show",container:b,data_attribute:"original",skip_invisible:!0,appear:null,load:null,placeholder:"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXYzh8+PB/AAffA0nNPuCLAAAAAElFTkSuQmCC"};return f&&(d!==f.failurelimit&&(f.failure_limit=f.failurelimit,delete f.failurelimit),d!==f.effectspeed&&(f.effect_speed=f.effectspeed,delete f.effectspeed),a.extend(j,f)),h=j.container===d||j.container===b?e:a(j.container),0===j.event.indexOf("scroll")&&h.bind(j.event,function(){return g()}),this.each(function(){var b=this,c=a(b);b.loaded=!1,(c.attr("src")===d||c.attr("src")===!1)&&c.is("img")&&c.attr("src",j.placeholder),c.one("appear",function(){if(!this.loaded){if(j.appear){var d=i.length;j.appear.call(b,d,j)};if(c.parent("picture").length>0&&check_support_webp()){c.attr("data-"+j.data_attribute,c.prev().attr("srcset"))};a("<img />").bind("load",function(){var d=c.attr("data-"+j.data_attribute);c.hide(),c.is("img")?c.attr("src",d):c.css("background-image","url('"+d+"')"),c[j.effect](j.effect_speed),b.loaded=!0;var e=a.grep(i,function(a){return!a.loaded});if(i=a(e),j.load){var f=i.length;j.load.call(b,f,j)}}).attr("src",c.attr("data-"+j.data_attribute))}}),0!==j.event.indexOf("scroll")&&c.bind(j.event,function(){b.loaded||c.trigger("appear")})}),e.bind("resize",function(){g()}),/(?:iphone|ipod|ipad).*os 5/gi.test(navigator.appVersion)&&e.bind("pageshow",function(b){b.originalEvent&&b.originalEvent.persisted&&i.each(function(){a(this).trigger("appear")})}),a(c).ready(function(){g()}),this},a.belowthefold=function(c,f){var g;return g=f.container===d||f.container===b?(b.innerHeight?b.innerHeight:e.height())+e.scrollTop():a(f.container).offset().top+a(f.container).height(),g<=a(c).offset().top-f.threshold},a.rightoffold=function(c,f){var g;return g=f.container===d||f.container===b?e.width()+e.scrollLeft():a(f.container).offset().left+a(f.container).width(),g<=a(c).offset().left-f.threshold},a.abovethetop=function(c,f){var g;return g=f.container===d||f.container===b?e.scrollTop():a(f.container).offset().top,g>=a(c).offset().top+f.threshold+a(c).height()},a.leftofbegin=function(c,f){var g;return g=f.container===d||f.container===b?e.scrollLeft():a(f.container).offset().left,g>=a(c).offset().left+f.threshold+a(c).width()},a.inviewport=function(b,c){return!(a.rightoffold(b,c)||a.leftofbegin(b,c)||a.belowthefold(b,c)||a.abovethetop(b,c))},a.extend(a.expr[":"],{"below-the-fold":function(b){return a.belowthefold(b,{threshold:0})},"above-the-top":function(b){return!a.belowthefold(b,{threshold:0})},"right-of-screen":function(b){return a.rightoffold(b,{threshold:0})},"left-of-screen":function(b){return!a.rightoffold(b,{threshold:0})},"in-viewport":function(b){return a.inviewport(b,{threshold:0})},"above-the-fold":function(b){return!a.belowthefold(b,{threshold:0})},"right-of-fold":function(b){return a.rightoffold(b,{threshold:0})},"left-of-fold":function(b){return!a.rightoffold(b,{threshold:0})}});function check_support_webp(){return document.createElement('canvas').toDataURL('image/webp').indexOf('data:image/webp')==0}}(jQuery,window,document);
 
-// clientAnimate.js
-$.fn.extend({clientAnimate:function(t){var i=$(this),n=$(window).height(),f=$(window).width(),o=$.extend({difference:60,toggle:false,className:"run",animatStop:992},t);i.each(function(t){var e=$(this).height(),i=$(this).offset().top;if(f>=o.animatStop?n>i:true){$(this).addClass(o.className)}});$(window).scroll(function(t){var e=$(this),a=e.scrollTop(),s=o.difference;i.each(function(t){var e=$(this).height(),i=$(this).offset().top;if(a+n>=i+s&&a<=i+e-s){if(!$(this).hasClass(o.className)){$(this).addClass(o.className)}}else if(a>i+e+s||a+n<i-s){if(o.toggle&&f>=o.animatStop){$(this).removeClass(o.className)}}})});return i}});$(function(){if(false&&$(window).innerWidth()>959){var e=["j-offset-top","j-offset-left","j-offset-right"];$(".f-box").each(function(){var t=$(this);if(!t.hasClass("no-animate")){t.addClass(e[Math.floor(e.length*Math.random())])}});$(".j-offset-top").clientAnimate();$(".j-offset-left").clientAnimate();$(".j-offset-right").clientAnimate()}$(".j-offset-top").clientAnimate();$(".j-offset-left").clientAnimate();$(".j-offset-right").clientAnimate();$(".j-fadein").clientAnimate()});
+// clientAnimate.js - 优化版本
+(function($) {
+    'use strict';
 
+    // 节流函数
+    function throttle(func, wait) {
+        var timeout;
+        var previous = 0;
+        return function() {
+            var context = this;
+            var args = arguments;
+            var now = Date.now();
+            var remaining = wait - (now - previous);
+            
+            if (remaining <= 0 || remaining > wait) {
+                if (timeout) {
+                    clearTimeout(timeout);
+                    timeout = null;
+                }
+                previous = now;
+                func.apply(context, args);
+            } else if (!timeout) {
+                timeout = setTimeout(function() {
+                    previous = Date.now();
+                    timeout = null;
+                    func.apply(context, args);
+                }, remaining);
+            }
+        };
+    }
+
+    $.fn.extend({
+        clientAnimate: function(options) {
+            var $elements = $(this);
+            var windowWidth = $(window).width();
+            var settings = $.extend({
+                difference: 60,
+                toggle: false,
+                className: 'run',
+                animatStop: 992
+            }, options);
+            
+            var scrollHandler;
+            var elementsArray = $elements.toArray();
+            
+            // 初始化检查
+            function checkInitialVisibility() {
+                var shouldAnimate = windowWidth >= settings.animatStop;
+                
+                if (shouldAnimate) {
+                    $elements.each(function() {
+                        var rect = this.getBoundingClientRect();
+                        if (rect.top < window.innerHeight && rect.bottom > 0) {
+                            $(this).addClass(settings.className);
+                        }
+                    });
+                }
+            }
+            
+            // 滚动处理函数
+            function handleScroll() {
+                var windowHeight = window.innerHeight;
+                var difference = settings.difference;
+                var shouldToggle = settings.toggle && windowWidth >= settings.animatStop;
+                
+                elementsArray.forEach(function(element) {
+                    var $el = $(element);
+                    var rect = element.getBoundingClientRect();
+                    var hasClass = $el.hasClass(settings.className);
+                    
+                    // 检查元素是否在视口中
+                    var isInViewport = rect.bottom > difference && 
+                                     rect.top < windowHeight - difference;
+                    
+                    var isOutOfViewport = rect.bottom < -difference || rect.top > windowHeight + difference;
+                    
+                    if (isInViewport) {
+                        if (!hasClass) {
+                            $el.addClass(settings.className);
+                        }
+                    } else if (isOutOfViewport && shouldToggle && hasClass) {
+                        $el.removeClass(settings.className);
+                    }
+                });
+            }
+            
+            // 使用节流函数绑定滚动事件
+            scrollHandler = throttle(handleScroll, 16); // 约60fps
+            
+            // 执行初始检查
+            checkInitialVisibility();
+            
+            // 绑定滚动事件
+            $(window).on('scroll.clientAnimate', scrollHandler);
+            
+            // 返回jQuery对象以支持链式调用
+            return $elements;
+        }
+    });
+
+    // 页面加载完成后初始化
+    $(function() {
+        // 初始化所有动画元素
+        $('.j-offset-top').clientAnimate();
+        $('.j-offset-left').clientAnimate();
+        $('.j-offset-right').clientAnimate();
+        $('.j-fadein').clientAnimate();
+    });
+
+})(jQuery);
 
 // placeholder.js
 $(function(){if(!placeholderSupport()){$("[placeholder]").focus(function(){var l=$(this);if(l.val()==l.attr("placeholder")){l.val("");l.removeClass("placeholder")}}).blur(function(){var l=$(this);if(l.val()==""||l.val()==l.attr("placeholder")){l.addClass("placeholder");l.val(l.attr("placeholder"))}}).blur()}});function placeholderSupport(){return"placeholder"in document.createElement("input")}
